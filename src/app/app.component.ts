0a9e9817ed92e2762f42e24731383169a32ead84
Philip Noebauer <philip.noebauer@86400.com.au>
2025-03-03 12:00:49 +1100

    add interval subscription

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = interval(1000)
+      .pipe()
+      .subscribe({
+        next: (value) => {
+          console.log(value);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+}
